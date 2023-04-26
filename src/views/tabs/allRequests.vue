4933f9d8ac4a2a0c42f2be657d713bbd34f05db1
tommytr25 <thong.truong@tcu.edu>
2023-04-26 11:31:45 -0500

    test

--- src/views/tabs/allRequests.vue
+++ src/views/tabs/allRequests.vue
@@ -1,7 +1,13 @@
 <template>
   <div>All Requests</div>
+  <div class="box box1">
+    <input type="text" id="input" class="input1" placeholder=" " required>
+    <label class="placeholder" for="input">First Name</label>
+  </div>
+  
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped></style>
+
