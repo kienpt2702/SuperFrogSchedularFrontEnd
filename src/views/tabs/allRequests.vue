d52a88c67357bbdf6d656c484223b0942253dc3a
tommytr25 <thong.truong@tcu.edu>
2023-04-27 18:29:28 -0500

    axios done for view request

--- src/views/tabs/allRequests.vue
+++ src/views/tabs/allRequests.vue
@@ -1,13 +1,62 @@
 <template>
-  <div>All Requests</div>
-  <div class="box box1">
-    <input type="text" id="input" class="input1" placeholder=" " required>
-    <label class="placeholder" for="input">First Name</label>
+  <div>
+    <p id="view-requests"> Requests </p>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email Address</th>
+          <th>Request Status</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="requests in computedRequests" :key="requests.id">
+          <td>{{ requests.id }}</td>
+          <td>{{ requests.customerFirstName }}</td>
+          <td>{{ requests.customerLastName }}</td>
+          <td>{{ requests.customerEmail }}</td>
+          <td>{{ requests.status }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-  
 </template>
 
-<script setup></script>
+<script>
+import axios from "axios";
 
-<style lang="scss" scoped></style>
-
+export default {
+  name: "ViewRequests",
+  data() {
+    return {
+      requests: [],
+    };
+  },
+  mounted() {
+    this.getRequests();
+  },
+  methods: {
+    getRequests() {
+     
+      axios
+        .get('http://localhost:8080/api/v1/requests')
+        .then(response => {
+          console.log(response.data.data);
+          this.requests = response.data.data;
+         
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+  },
+  computed: {
+    computedRequests() {
+      return this.requests;
+    },
+  },
+};
+</script>
