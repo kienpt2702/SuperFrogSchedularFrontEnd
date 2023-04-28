05ef51c5d08856e50ebef7feffd419639b86e607
Taylor Griffin <104051323+imtaylorgriffin@users.noreply.github.com>
2023-04-27 22:26:35 -0500

    added CSS to Tommy's allRequests.vue, added some page logic ideas too. Working on newRequest.vue

--- src/views/tabs/allRequests.vue
+++ src/views/tabs/allRequests.vue
@@ -4,9 +4,9 @@
     <table>
       <thead>
         <tr>
-          <th>ID</th>
-          <th>First Name</th>
-          <th>Last Name</th>
+          <th>Event Title</th>
+          <th>Event Description</th>
+          <th>Customer Name</th>
           <th>Email Address</th>
           <th>Request Status</th>
           
@@ -14,16 +14,26 @@
       </thead>
       <tbody>
         <tr v-for="requests in computedRequests" :key="requests.id">
-          <td>{{ requests.id }}</td>
-          <td>{{ requests.customerFirstName }}</td>
-          <td>{{ requests.customerLastName }}</td>
+          <td>{{ requests.eventTitle }}</td>
+          <td>{{ requests.eventDescription }}</td>
+          <!--<td>{{ requests.id }}</td>-->
+          <td>{{ requests.customerFirstName +" "+  requests.customerLastName }}</td>
           <td>{{ requests.customerEmail }}</td>
           <td>{{ requests.status }}</td>
         </tr>
       </tbody>
     </table>
+    <!--button added-->
+    <button @click = "nextPage" v-if="page < lastPage">Next Page</button>
+    <button @click = "prevPage" v-if="showPrevButton">Previous Page</button>
   </div>
 </template>
+
+
+<!-- eventually we should plan on making only requests that are Approved status visible, 
+  maybe with a button next to it instead of having the Status column, when clicked will route to vue page that will sign up a super frog for the request-->
+
+
 
 <script>
 import axios from "axios";
@@ -33,6 +43,8 @@ export default {
   data() {
     return {
       requests: [],
+      page: 1,
+      showPrevButton:false, 
     };
   },
   mounted() {
@@ -46,17 +58,93 @@ export default {
         .then(response => {
           console.log(response.data.data);
           this.requests = response.data.data;
+          this.showPrevButton = false;
          
         })
         .catch(error => {
           console.log(error);
         });
     },
+    nextPage(){
+      this.page++;
+      if (this.page === this.lastPage) { 
+        this.showNextButton = false;
+      }
+      if (this.page > 1) {
+        this.showPrevButton = true;
+      }
+      
+    },
+    prevPage(){
+      
+      if(this.page>1){
+        this.page--;
+      if(this.page === 1) {
+        this.showPrevButton=false;
+      }
+      if(!this.showPrevButton){
+        this.showNextButton=true;
+      }
+      }
+    }
   },
   computed: {
     computedRequests() {
-      return this.requests;
+      const startIndex = (this.page - 1)*15;
+      const endIndex = startIndex +15;
+      return this.requests.slice(startIndex,endIndex);
     },
+    lastPage(){
+      return Math.ceil(this.requests.length/15);
+    }
   },
 };
 </script>
+
+<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th,
+td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+th {
+  background-color: #f2f2f2;
+  font-weight: bold;
+}
+
+tr:nth-child(even) {
+  background-color: #e6e6e6;
+}
+
+button {
+  background-color: #4CAF50; 
+  border: none; 
+  color: white; 
+  padding: 10px 20px; /* Some padding */
+  text-align: center; 
+  text-decoration: none; 
+  display: inline-block; 
+  font-size: 16px; 
+  margin-top: 10px;
+  margin-right: 10px;
+  cursor: pointer; 
+  transition-duration: 0.4s;
+}
+
+button:hover {
+  background-color: #4D1979;
+}
+
+.prev-button.hidden {
+  opacity: 0; /* Set opacity to 0 when hidden */
+}
+
+
+</style>
--- src/views/tabs/newRequest.vue
+++ src/views/tabs/newRequest.vue
@@ -1,7 +1,133 @@
 <template>
-  <div>New Request</div>
+  <div class="form-container">
+    <h2 class="form-header">Event Registration Form</h2>
+    <form @submit.prevent="submitForm">
+      <label for="event-title">Event Title:</label>
+      <input type="text" id="event-title" name="event-title">
+  
+      <label for="event-date">Event Date:</label>
+      <input type="date" id="event-date" name="event-date">
+  
+      <label for="event-time">Event Time:</label>
+      <input type="time" id="event-time" name="event-time">
+  
+      <label for="event-address">Event Address:</label>
+      <input type="text" id="event-address" name="event-address">
+  
+      <label for="event-description">Event Description:</label>
+      <textarea id="event-description" name="event-description" rows="5" cols="40"></textarea>
+  
+      <label for="customer-first-name">Customer First Name:</label>
+      <input type="text" id="customer-first-name" name="customer-first-name">
+  
+      <label for="customer-last-name">Customer Last Name:</label>
+      <input type="text" id="customer-last-name" name="customer-last-name">
+  
+      <label for="customer-email">Customer Email:</label>
+      <input type="email" id="customer-email" name="customer-email">
+  
+      <label for="customer-phone">Customer Phone Number:</label>
+      <input type="tel" id="customer-phone" name="customer-phone">
+  
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+  
+
 </template>
 
-<script setup></script>
 
-<style lang="scss" scoped></style>
+<script>
+import axios from "axios";
+export default {
+  methods: {
+    async submitForm() {
+      // get the form data
+      const formData = new FormData(document.querySelector("form"));
+
+      try {
+        // send the form data to the API endpoint
+        const response = await axios.post("http://localhost:8080/api/v1/requests", formData);
+        console.log(formData);
+
+        // handle the response
+        if (response.status === 200) {
+          alert("Form submitted successfully!");
+          console.log(response.data);
+        } else {
+          alert("Form submission failed.");
+          console.error(response.data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+
+</script>
+
+
+
+<style lang="scss" scoped>
+  /* Style the form container */
+  .form-container {
+    width: 80%;
+    margin: 20px auto;
+    background-color: #f2f2f2;
+    padding: 10px;
+    border-radius: 10px;
+}
+
+/* Style the form fields */
+.form-container input[type=text],
+.form-container input[type=date],
+.form-container input[type=time],
+.form-container input[type=email],
+.form-container input[type=tel],
+.form-container textarea {
+    width: 100%;
+    padding: 8px;
+    margin: 4px 0;
+    box-sizing: border-box;
+    border: none;
+    border-radius: 4px;
+    background-color: #fff;
+    font-size: 14px;
+}
+
+/* Style the form labels */
+.form-container label {
+    font-size: 16px;
+    display: block;
+    margin-bottom: 5px;
+}
+
+/* Style the form submit button */
+.form-container input[type=submit] {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px 16px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 16px;
+}
+
+/* Change the color of the submit button on hover */
+.form-container input[type=submit]:hover {
+    background-color: #45a049;
+}
+
+/* Style the form header */
+.form-header {
+    text-align: center;
+    font-size: 1.8rem;
+    margin-bottom: 10px;
+}
+
+
+
+</style>
+
+
