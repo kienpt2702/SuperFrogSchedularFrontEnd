384b861b3dd184d5754d778e1431081de4a5a4c1
drakenevadie19 <drakennievadje19@gmail.com>
2023-05-01 09:20:21 -0500

    Finish the Navigation bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,32 @@
 <script>
+import cache from '@/utils/cacheUtils';
 import { RouterLink } from 'vue-router';
+import { logout } from '../utils/userLoginUtils';
+var isLoggedIn = false;
+export default {
+    data() {
+        return {
+            isLoggedIn: (cache.get("login_token") != null),
+        }
+    },
+    methods: {
+        whenLogout: function() {
+            logout();
+        }
+    }
+}
 </script>
 
 <template>
   <nav class="navigation">
-    <div style="display: flex; justify-content: end;">
+    <div v-if="isLoggedIn">
+        <div class="imageHolder">
+            <img src="../views/SuperFrogforLoginPage.jpg" alt="Image" class="image">
+            <button @click="whenLogout">Logout</button>
+        </div>
+        
+    </div> 
+    <div v-else style="display: flex; justify-content: end;">
         <div class="loginNav">
           <RouterLink :to="{ name: 'login' }"><p class="login-text">Staff Login</p></RouterLink>
         </div>
@@ -13,6 +35,15 @@ import { RouterLink } from 'vue-router';
 </template>
 
 <style lang="scss" scoped>
+    .imageHolder {
+        display: flex;
+        justify-content: end;
+    }
+    .image {
+        height: 80px;
+        width: 80px;
+        border-radius: 200px;
+    }
 .navigation {
     width: 100%;
   background-color: #501c7c;
